961d171e482266df1ebad6cb36f58ffdeed0917f
Norihide okada <nokada@harsglobal.com>
2026-03-15 06:46:02 +0900

    cc

--- src/app/alcohol/[id]/page.tsx
+++ src/app/alcohol/[id]/page.tsx
@@ -85,7 +85,7 @@ export default function ReviewDetailPage() {
         className="absolute inset-0 bg-cover"
         style={{
     backgroundImage: "url('/review-bg.jpg')",
-    backgroundPosition: "center 50%",
+    backgroundPosition: "center 100%",
   }}
       />
       <div className="absolute inset-0 bg-black/70" />
